3cfd594755aacdabe5d7a087aef8d1dc8c645519
alex Davis <alex@alex2006hw.com>
2017-02-28 00:44:03 -0800

    revert-https://github.com/yonahforst/react-native-heading

--- ReactNativeHeading.h
+++ ReactNativeHeading.h
@@ -4,9 +4,8 @@
 //
 //  Created by Yonah Forst on 18/02/16.
 //  Copyright © 2016 Yonah Forst. All rights reserved.
-//  Updated by Alex2006hw on 20/02/17.
-
-#import "RCTBridgeModule.h"
+//
+#import <React/RCTBridgeModule.h>
 
 #import <Foundation/Foundation.h>
 
